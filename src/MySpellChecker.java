bb99edcfdb5ef3ec7a10686a2013a0bf410d4d3c
Attagoat <attadoesmystiv@gmail.com>
2024-12-12 10:57:24 -0600

    TODO: finish temp files and store in main files

--- src/MySpellChecker.java
+++ src/MySpellChecker.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 
+import java.io.FileWriter;
 import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -13,7 +14,10 @@ public class MySpellChecker {
         List<MyWord> ignoreList = new ArrayList<MyWord>();
         Scanner scanley = new Scanner(System.in);
         try(Scanner dictScanner = new Scanner(new File(args[0])); Scanner inputScanner = new Scanner(new File(args[1]));
-            PrintWriter writer = new PrintWriter(new File("ElohimTextCorrections.txt"));){
+            PrintWriter correctionsWriter = new PrintWriter(new File("tempCorrect.txt"));
+            PrintWriter dictWriter = new PrintWriter(new File("tempDict.txt"));
+            ){
+
             while(dictScanner.hasNext()){
                 MyWord dictWord = new MyWord(dictScanner.next().toCharArray());
                 dictionary.add(dictWord);
@@ -57,18 +61,27 @@ public class MySpellChecker {
                             dictionary.add(inputList.get(i));
                         }
                     }
-                    writer.print(inputList.get(i) + " ");
+                    correctionsWriter.print(inputList.get(i) + " ");
                 }
 
-                writer.println();
+                correctionsWriter.println();
                 lineNumber++;
                 inputList.clear();
             }
+            for (int i = 0; i < dictionary.size(); i++) {
+                dictWriter.println(dictionary.get(i));
 
+            }
 
 
         } catch(FileNotFoundException e){
             System.out.println("Usage: (Dictionary File) (Input Location)");
+
+        }
+        //#TODO: Copy over temp files and store them in the other one
+        try(
+                Scanner = new Scanner(new File("tempCorrect.txt"));
+                ){
 
         }
 
